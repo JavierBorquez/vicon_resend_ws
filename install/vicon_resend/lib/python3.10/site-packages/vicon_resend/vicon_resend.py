88e3ef1610a8e04d56a1ad0b87a9243f86f7af16
Javier <javierbo@usc.edu>
2024-08-12 16:26:44 -0700

    updated topic name for compatibility

--- install/vicon_resend/lib/python3.10/site-packages/vicon_resend/vicon_resend.py
+++ install/vicon_resend/lib/python3.10/site-packages/vicon_resend/vicon_resend.py
@@ -15,7 +15,7 @@ sock.bind(server_address)
 class OdometryPublisher(Node):
     def __init__(self):
         super().__init__('odometry_publisher')
-        self.publisher_ = self.create_publisher(Odometry, 'odometry', 10)
+        self.publisher_ = self.create_publisher(Odometry, 'odom', 10)
         timer_period = 0.05  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
